0159db20165041e6ff040d55ae2968e63337d3da
fkgrkyr <fkgrkyr@gmail.com>
2025-09-13 19:56:27 +0900

    feat: localStorage로 할일 저장

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 // 할 일을 저장할 객체
 let todos = {};
 
+// localStorage에서 할 일 불러오기
+function loadTodos() {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+        todos = JSON.parse(savedTodos);
+    }
+}
+
+// localStorage에 할 일 저장하기
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 // 달력 표시 함수
 function showCalendar() {
     const calendarDates = document.getElementById('calendarDates');
@@ -23,7 +36,7 @@ function showCalendar() {
             dateElement.className = 'date today';
         }
 
-        // 할 일이 있는 날짜면 hasTodos 클래스 추가 -> 노란색 배경
+        // 할 일 있는 날짜면 hasTodos 클래스 추가 -> 노란색 배경
         if (todos[day] && todos[day].length > 0) {
             dateElement.className += ' hasTodos';
         }
@@ -40,6 +53,9 @@ function showCalendar() {
 
                 todos[day].push(todo);  // 할 일 추가
                 alert('할 일이 추가되었습니다!');
+                
+                // localStorage에 저장
+                saveTodos();
                 console.log('현재 할 일 목록:', todos);
 
                 // 할 일 추가되면 달력 다시 그리기
@@ -51,4 +67,6 @@ function showCalendar() {
     }
 }
 
-showCalendar();
+// 페이지 시작할 때 실행
+loadTodos();  // 저장된 할 일 불러오기
+showCalendar(); // 달력 표시
